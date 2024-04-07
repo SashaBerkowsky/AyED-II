0f9624c3d9f2e8dbc4c88efeec2744c1b84e0311
Sasha Berkowsky <s.nberkowsky@gmail.com>
2024-04-07 01:54:49 -0300

    TP1 & practica2

---
+++ Prácticas/Práctica 2 - Debugging/src/test/java/aed/CoberturaTests.java
@@ -0,0 +1,26 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CoberturaTests {
+    Cobertura cobertura = new Cobertura();
+
+    @Test
+    void testFizzBuzz() {
+        assertEquals("A","B");
+    }
+
+    @Test
+    void testNumeroCombinatorio() {
+        assertTrue(false);
+    }
+
+    @Test
+    void testRepeticionesConsecutivas() {
+        assertTrue(false);
+    }
+}
